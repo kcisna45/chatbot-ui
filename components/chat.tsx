31b3488b8fbec016ac346022580054c259012e80
kcisna45 <kcisna45@gmail.com>
2025-07-04 20:19:52 -0500

    Create chat.tsx

---
+++ components/chat.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+
+export default function Chat() {
+return (
+<div style={{ padding: '2rem' }}>
+<h1>Welcome to SourceField Chat</h1>
+<p>This is a placeholder chat interface.</p>
+</div>
+)
+}
