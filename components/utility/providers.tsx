4c95fc6195cd94dcf6a6940b3ad8c500c3175b9f
kcisna45 <kcisna45@gmail.com>
2025-08-01 20:10:29 -0500

    Fix: correct TooltipProvider import and clean Providers component

--- components/utility/providers.tsx
+++ components/utility/providers.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import { TooltipProvider } from "@/components/ui/with-tooltip"
+import { TooltipProvider } from "@/components/ui/tooltip"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import type { ThemeProviderProps } from "next-themes/dist/types"
 import { FC } from "react"
 
 export const Providers: FC<ThemeProviderProps> = ({ children, ...props }) => {
-  return (
-    <NextThemesProvider {...props}>
-      <TooltipProvider>{children}</TooltipProvider>
-    </NextThemesProvider>
-  )
+return (
+<NextThemesProvider {...props}>
+<TooltipProvider>
+{children}
+</TooltipProvider>
+</NextThemesProvider>
+)
 }
