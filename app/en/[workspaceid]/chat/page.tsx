9f353aebacd197248fc9092459506ab4fb872c09
Kaylee Cisna <kcisna45@gmail.com>
2026-05-04 18:21:00 -0500

    fix chat page component structure

--- app/en/[workspaceid]/chat/page.tsx
+++ app/en/[workspaceid]/chat/page.tsx
@@ -1,34 +1,24 @@
 "use client"
 
 import { ChatHelp } from "@/components/chat/chat-help"
-import { useChatHandler } from "@/components/chat/chat-hooks/use-chat-handler"
 import { ChatInput } from "@/components/chat/chat-input"
 import { ChatSettings } from "@/components/chat/chat-settings"
 import { ChatUI } from "@/components/chat/chat-ui"
 import { QuickSettings } from "@/components/chat/quick-settings"
 import { Brand } from "@/components/ui/brand"
 import { ChatbotUIContext } from "@/context/context"
-import useHotkey from "@/lib/hooks/use-hotkey"
 import { useTheme } from "next-themes"
 import { useContext } from "react"
 
 export default function ChatPage() {
-  useHotkey("o", () => handleNewChat())
-  useHotkey("l", () => {
-    handleFocusChatInput()
-  })
-
   const { chatMessages } = useContext(ChatbotUIContext)
-
-  const { handleNewChat, handleFocusChatInput } = useChatHandler()
-
   const { theme } = useTheme()
 
   return (
     <>
       {chatMessages.length === 0 ? (
         <div className="relative flex h-full flex-col items-center justify-center">
-          <div className="top-50% left-50% -translate-x-50% -translate-y-50% absolute mb-20">
+          <div className="absolute left-1/2 top-1/2 mb-20 -translate-x-1/2 -translate-y-1/2">
             <Brand theme={theme === "dark" ? "dark" : "light"} />
           </div>
 
