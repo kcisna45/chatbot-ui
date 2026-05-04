3f1e6c51d09022397bc7abbaa85ad89314d175d6
Kaylee Cisna <kcisna45@gmail.com>
2026-05-03 19:49:50 -0500

    disable PWA

--- next.config.js
+++ next.config.js
@@ -2,8 +2,10 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"
 })
 
-const withPWA = require("next-pwa")({
-  dest: "public"
+const withPWA = (config) => config
+
+module.exports = withPWA({
+// your existing config
 })
 
 module.exports = withBundleAnalyzer(
